966184530105e96a251956c4a7e192839208efec
vector26 <mrdev2604@gmail.com>
2021-05-07 14:21:28 +0530

    Post Count for profile added

--- src/Profile/models.py
+++ src/Profile/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from Feed.models import PostContent
 import datetime
 # Create your models here.
 class Profile(models.Model):
